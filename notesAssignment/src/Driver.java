0a721c74243c685dfcc70e88de6d18921aaee9f7
Ron Rizzo <churchmouth13@gmail.com>
2023-02-16 12:19:19 -0500

    Adds example JFugue code for testing out pattern functionality

--- notesAssignment/src/Driver.java
+++ notesAssignment/src/Driver.java
@@ -1,4 +1,5 @@
 import org.jfugue.player.Player;
+import org.jfugue.pattern.Pattern;
 
 public class Driver {
 
@@ -24,11 +25,15 @@ public class Driver {
 
         Note[] DAG = {D3, A3, G3};
         Note[] CDG = {C4, D4, G3};
+        Pattern p1 = new Pattern("V0 I[Piano] Eq Ch. | Eq Ch. | Dq Eq Dq Cq");
+        Pattern p2 = new Pattern("V1 I[Flute] Rw     | Rw     | GmajQQQ  CmajQ");
 
         Player player = new Player();
 
         playNotes(DAG);
         playNotes(CDG);
+        A4.setLength("w");
         player.play(A4.toJfugue());
+        player.play(p1, p2);
     }
 }

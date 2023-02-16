90bc6fc6d1603054ef8f9de97dcd78e7c684f5d0
Ron Rizzo <churchmouth13@gmail.com>
2023-02-16 10:51:28 -0500

    Create playNotes method to play an array of notes Create two arrays of notes to test Note class

--- notesAssignment/src/Driver.java
+++ notesAssignment/src/Driver.java
@@ -1,8 +1,26 @@
 import org.jfugue.player.Player;
 
 public class Driver {
+
+    public static void playNotes(Note[] notes) {
+        Player player = new Player();
+        for(int i = 0; i< notes.length;i++){
+            player.play(notes[i].toJfugue());
+        }
+    }
    
     public static void main(String[] args) throws Exception {
-        
+        Note D3 = new Note("h", -19);
+        Note A3 = new Note("q", -12);
+        Note G3 = new Note("q", -14);
+
+        Note C4 = new Note("h", -9);
+        Note D4 = new Note("q", -7);
+        Note A4 = new Note();
+
+        Note[] DAG = {D3, A3, G3};
+        Note[] CDG = {C4, D4, G3};
+
+
     }
 }

1a4b015c7d8ae434260f2d13bf2fbc5db4410cf0
Ron Rizzo <churchmouth13@gmail.com>
2023-03-20 19:37:35 -0400

    Fleshes out testing code
    Adds code to create all the notes of the C scale starting on C3 and
    ending on C4. Demonstrates compareTos effectiveness on its own and
    in the context of sorting an ArrayList of Note objects. Creates a
    pattern from c3Scale for testing with JFugue.

--- notesAssignment/src/Driver.java
+++ notesAssignment/src/Driver.java
@@ -1,32 +1,58 @@
 import org.jfugue.player.Player;
 import org.jfugue.pattern.Pattern;
+import java.util.*;
+
 
 public class Driver {
-    
+ 
     /** Uses a for loop to convert an array of Notes into a pattern for JFugue
      * @param measure Array of Note objects
      * @return pattern for playing through JFugue
      */
-    public static Pattern toPattern(Note[] measure) {
+    
+    public static Pattern toPattern(ArrayList<Note> measure) {
         Pattern output = new Pattern();
-        for(int i = 0; i < measure.length; i++) {
-            output.add(measure[i].toJfugue());
+        for(Note note : measure) {
+            output.add(note.toJfugue());
         }
         output.add("|");
         return output;
     }
+    
    
     public static void main(String[] args) throws Exception {
         /*
          * Creates a seven different Note objects for use in testing
          */
-        Note D3 = new Note("h", -19);
+        Note C3 = new Note("q", -21);
         Note A3 = new Note("q", -12);
         Note G3 = new Note("q", -14);
-        Note C4 = new Note("h", -9);
-        Note D4 = new Note("q", -7);
-        Note A4 = new Note();
+        Note B3 = new Note("q", -10);
+        Note D3 = new Note("q", -19);
+        Note F3 = new Note("q", -16);
+        Note E3 = new Note("q", -17);
     
+        ArrayList<Note> c3Scale = new ArrayList<>();
+        c3Scale.add(C3);
+        c3Scale.add(G3);
+        c3Scale.add(A3);
+        c3Scale.add(F3);
+        c3Scale.add(E3);
+        c3Scale.add(D3);
+        c3Scale.add(B3);
+        
+        Collections.sort(c3Scale);
+
+        int compTest = C3.compareTo(E3);
+        if(compTest < 0) {
+            System.out.println("C3 is less than E3");
+        } else if (compTest > 0) {
+            System.out.println("C3 is greater than E3");
+        }
+
+        Pattern scale = toPattern(c3Scale).setVoice(1).setInstrument("Piano");
+        
+
         /*
          * Creates two arrays of Note objects
         
